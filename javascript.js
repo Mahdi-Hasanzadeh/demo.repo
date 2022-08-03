631cd47b7a9a1703fa11c09a2ce14fe49cff02f2
Mahdi Hasanzadeh <Mahdi.h2619@gmail.com>
2022-08-03 19:20:38 +0430

    New Fonts Added

--- javascript.js
+++ javascript.js
@@ -1,11 +1,18 @@
 function designedName(){
     var name = document.getElementById("name").value;
     if(name != ""){
-        document.getElementById("example").innerHTML = name;
+        document.getElementById("ex").innerHTML = name;
     }
 }
 function changeFont(){
     var selectedFont = document.getElementById("fontSelection").value;
    // alert(typeof selectedFont);
     document.getElementById("ex").style.fontFamily = selectedFont;
+    document.getElementById("label").innerHTML = selectedFont + " Font";
+    if(selectedFont == "AligotDeMirabelle"){
+        document.getElementById("ex").style.fontSize = '25vh';
+    }
+    else{
+        document.getElementById("ex").style.fontSize = '9vh';
+    }
 }
